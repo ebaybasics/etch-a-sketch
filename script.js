1cf893abe6ab0ccc47697b9df0e2858fbbad6b20
homegrownhack <kccleancarpets@gmail.com>
2024-01-19 11:31:40 -0600

    Add functionality: Adjust num of cells, eventlisteners to change color of cells

--- script.js
+++ script.js
@@ -1,16 +1,73 @@
 'use-strict'
+const NUMCELLS = 16;
 
 const gameBox = document.querySelector('.game-box');
+const reset = document.querySelector('.reset');
+const penLarge = document.querySelector('.pen-large');
 
-const newGrid = function(numRows, numColumns) {
-    for (let i = 0; i <numRows; i++) {
-        for (let j = 0; j < numColumns; j++) {
+
+const newGrid = function(gridSize) {
+    
+    for (let i = 0; i <gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
             const gridCell = document.createElement('div');
             gridCell.classList.add('grid-cell');
             gameBox.appendChild(gridCell);
         };
 
     };
+    
 };
 
-newGrid(16,16)
+const changeGrid = function() {
+    const numCells = NUMCELLS;
+    const flexB = ((-.05*numCells+6));
+    // newGrid(numCells);
+
+    const gridCells = document.querySelectorAll('.grid-cell');
+
+    gridCells.forEach((gridCell, key, nodeList)=> {
+        let cell = nodeList[`${key}`]
+
+        cell.style.flexBasis = `${flexB}%`;
+
+
+        
+        nodeList[`${key}`].addEventListener('mouseover', (e) => {
+            e.preventDefault();
+
+            if (e.buttons == 1) nodeList[`${key}`].style.backgroundColor = 'red';
+        })
+
+    });
+
+
+
+};
+
+newGrid(NUMCELLS);
+changeGrid();
+
+reset.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const gridCells = document.querySelectorAll('.grid-cell');
+    gridCells.forEach((cell) => {
+        cell.style.backgroundColor = '#99AABB';
+    })
+    
+});
+
+penLarge.addEventListener('click', (e) => {
+    e.preventDefault();
+    const gridCells = document.querySelectorAll('.grid-cell');
+    gridCells.forEach(cell => {
+        // console.log(window.getComputedStyle(cell).backgroundColor)
+        if (window.getComputedStyle(cell).backgroundColor == 'rgb(153 , 170,187') {
+            console.log('blue')
+        }
+    })
+
+})
+
+
